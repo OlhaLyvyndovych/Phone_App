5de4dcc50bfbcda2df748cb9e60186611fe4eefb
Olha Lyvyndovych <olha.lyvyndovych@gmail.com>
2019-04-05 11:09:54 +0300

    modified js files by creating components

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
+import PhonesPage from './phones/phones-page.js';
 
+//const containerEl = document.querySelector('[data-page-container]');
+const phonesPage = new PhonesPage( {
+  element: document.querySelector('[data-page-container]')
+} )
