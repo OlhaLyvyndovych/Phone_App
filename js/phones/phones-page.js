4cac9a0638f3c36b0da266e6127e1031ad13f78d
Olha Lyvyndovych <olha.lyvyndovych@gmail.com>
2019-04-10 13:54:25 +0300

    The recent changes 08/04 added

--- js/phones/phones-page.js
+++ js/phones/phones-page.js
@@ -1,55 +1,74 @@
 import PhonesCatalog from './components/phones-catalog.js';
+import PhoneViewer from './components/phone-viewer.js';
 import PhonesService from './services/phones-service.js';
 
 export default class PhonesPage {
-  constructor({ element }) {
-    this._element = element,
-    this._render();
+    constructor({ element }) {
+        this._element = element;
+        this._render();
 
-    this._catalog = new PhonesCatalog({
-      element: this._element.querySelector('[data-phone-catalog]'),
-      phones: PhonesService.getAll()
+        this._initCatalog();
+        this._initViewer();
 
-    });
-  }
+    }
 
-  _render() {
-    this._element.innerHTML = `
-    <div class="row">
+    _initCatalog() {
+        this._catalog = new PhonesCatalog({
+            element: this._element.querySelector('[data-component="phone-catalog"]'),
+            phones: PhonesService.getAll()
+        })
 
-      <!--Sidebar-->
-      <div class="col-md-2">
-        <section>
-          <p>
-            Search:
-            <input>
-          </p>
+        this._catalog.subscribe('phone-selected', (id) => {
+            console.log('Selected: ', id);
+            const phoneDetails = PhonesService.getById(id);
+            this._catalog.hide();
+            this._viewer.show(phoneDetails);
+        })
+    }
 
-          <p>
-            Sort by:
-            <select>
-              <option value="name">Alphabetical</option>
-              <option value="age">Newest</option>
-            </select>
-          </p>
-        </section>
+    _initViewer() {
+        this._viewer = new PhoneViewer({
+            element: this._element.querySelector('[data-component="phone-viewer"]')
+        })
 
-        <section>
-          <p>Shopping Cart</p>
-          <ul>
-            <li>Phone 1</li>
-            <li>Phone 2</li>
-            <li>Phone 3</li>
-          </ul>
-        </section>
-      </div>
+        this._viewer.subscribe('back', () => {
+            this._catalog.show();
+            this._viewer.hide();
+        })
+    }
 
-      <!--Main content-->
-      <div class="col-md-10">
-        <div data-phone-catalog>
+    _render() {
+        this._element.innerHTML = `
+        <div class="row">
+        <!--Sidebar-->
+        <div class="col-md-2">
+            <section>
+            <p>
+                Search:
+                <input>
+            </p>
+            <p>
+                Sort by:
+                <select>
+                <option value="name">Alphabetical</option>
+                <option value="age">Newest</option>
+                </select>
+            </p>
+            </section>
+            <section>
+            <p>Shopping Cart</p>
+            <ul>
+                <li>Phone 1</li>
+                <li>Phone 2</li>
+                <li>Phone 3</li>
+            </ul>
+            </section>
         </div>
-      </div>
-    </div>
-    `
-  }
+        <!--Main content-->
+        <div class="col-md-10">
+            <div data-component="phone-viewer"></div>
+            <div data-component="phone-catalog"></div>
+        </div>
+        </div>`
+    }
 }
